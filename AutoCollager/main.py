84aa993d1d657ee0c9d5d736c7ae61e662315497
Noah Lobbe <the.inventer@icloud.com>
2024-11-27 20:07:52 +1030

    Realised window title had a hypen meaning it wasn't correct. Eg. was Auto-Collager 3.0, needed to be AutoCollager 3.0

--- AutoCollager/main.py
+++ AutoCollager/main.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     print("running...")
     VERSION_STR = "3.0"
 
-    App = Collager("Auto-Collager", VERSION_STR)
+    App = Collager("AutoCollager", VERSION_STR)
     App.run()
 
